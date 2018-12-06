bf1a0af6ec943c168e20a1e7811c94d6b8c4c405
唐恒智 <touhisashisatoshi@tanghengzhis-MacBook-Pro.local>
2018-12-06 16:18:08 +0800

    update swagger v3

--- controllers/SwaggerController.php
+++ controllers/SwaggerController.php
@@ -5,11 +5,20 @@ namespace tanghengzhi\swagger\controllers;
 use yii\rest\Controller;
 use Yii;
 
+/**
+ * @OA\Info(title="My First API", version="0.1")
+ */
 class SwaggerController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/swagger/swagger/json",
+     *     @OA\Response(response="200", description="An example resource")
+     * )
+     */
     public function actionJson()
     {
-        $params = Yii::$app->params['apidoc']??[];
+        $params = Yii::$app->params['swagger'] ?? [];
         $doc_dir[] = __DIR__;
         if(isset($params['scan_dir']) && is_array($params['scan_dir']))
         {
@@ -20,12 +29,13 @@ class SwaggerController extends Controller
         }
         try
         {
-            $swagger =  \Swagger\scan($doc_dir);
-            echo ($swagger);exit;
+            $openapi = \OpenApi\scan($doc_dir);
+            header('Content-Type: application/json');
+            echo $openapi->toJson();
         }
         catch(\Exception $exception)
         {
-            echo json_encode([$exception->getFile(),$exception->getMessage(),$exception->getLine()]);
+            echo json_encode([$exception->getFile(), $exception->getMessage(), $exception->getLine()]);
         }
     }
 }
